8661b8b0879b776d20378a34a49840c8979f511a
Michał Przyłucki <michalprzyl@gmail.com>
2024-08-22 18:01:03 +0200

    Finished program working with input

--- main.c
+++ main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <string.h>
-
-#define N 5
 
 void print_array(int *arr, int size) {
   printf("Printing array:\n");
@@ -14,21 +11,14 @@ void print_array(int *arr, int size) {
 
 int *create_new_smaller_arr(int *arr, int size) {
   int *new_smaller_arr = (int *)malloc((size - 1) * sizeof(int));
-  // printf("Arr argument in create_new_smaller_arr function\n");
-  // print_array(arr, size);
   for (int i = 0; i < size - 1; i++) {
     if (arr[i] > arr[i + 1]) {
-      // printf("assigning left value of: %d\n", arr[i]);
       new_smaller_arr[i] = arr[i];
     } else {
-      // printf("assigning left value of: %d\n", arr[i + 1]);
       new_smaller_arr[i] = arr[i + 1];
     }
   }
-  //  4 6 6 2
-  // 4 3 6 2 1
-  // printf("new_smaller_arr before returning: \n");
-  print_array(new_smaller_arr, size - 1);
+  // print_array(new_smaller_arr, size - 1);
   return new_smaller_arr;
 }
 
@@ -43,22 +33,19 @@ int get_sum_of_arr(int *arr, int size) {
 
 int main() {
 
-  printf("Starting...\n");
+  int N;
+  scanf("%d", &N);
+  printf("\nStarting...\n");
 
-  // int base[N] = {1, 2, 3, 4, 5};
-  // int base[N] = {1, 10, 1, 10, 1, 10};
-  int base[N] = {4,3,6,2,1};
+  int *base = (int *)malloc(N * sizeof(int));
 
+  // Input for base
+  for (int i = 0; i < N; i++) {
+    scanf("%d", &base[i]);
+  }
+
+  // Initial values
   int output_sum = 0;
-
-  // for (int i = 0; i < N; i++){
-  //   output_sum += base[i];
-  // }
-
-  // dla kazdego poziomu
-  // stworz mniejsza arrajke
-  // znajdz mniejsza arrajke
-  // przypisz mniejsza arrajke
   int *current_base_arr = base;
   int current_size = N;
 
@@ -67,14 +54,12 @@ int main() {
     int *new_arr = create_new_smaller_arr(current_base_arr, current_size);
     int arr_value = get_sum_of_arr(new_arr, current_size - 1);
     output_sum += arr_value;
-    printf("Adding %d to output sum\n", arr_value);
 
-    if (i>0){
+    if (i > 0) {
       free(current_base_arr);
     }
     current_base_arr = new_arr;
     current_size--;
-
   }
   free(current_base_arr);
   printf("output sum: %d\n", output_sum);
